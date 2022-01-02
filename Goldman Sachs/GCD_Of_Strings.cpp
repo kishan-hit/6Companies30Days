acef93c69f771ab4507e73766d7c77e68a90bd93
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-01-02 23:11:10 +0530

    Create GCD_Of_Strings.cpp

---
+++ Goldman Sachs/GCD_Of_Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //Time Complexity: O(logN),N is the length of bigger string
+    //Space Complexity: O(1)
+    
+    string gcdOfStrings(string str1, string str2) {
+        if(str2.length()>str1.length()){
+            return gcdOfStrings(str2,str1);
+        }
+        else if(str2.empty()){
+            return str1;
+        }
+        else if(str1.substr(0,str2.length())!=str2){
+            return "";
+        }
+        return gcdOfStrings(str1.substr(str2.length()),str2);
+    }
+};
