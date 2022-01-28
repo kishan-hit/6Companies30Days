f3287519dcc897b54611377e5489f43acb7c57aa
Kishan Kumar <76576890+kishan-hit@users.noreply.github.com>
2022-01-29 00:14:31 +0530

    Create Remove_Colored_Pieces.cpp

---
+++ Walmart/Remove_Colored_Pieces.cpp
@@ -0,0 +1,13 @@
+bool winnerOfGame(string colors) {
+        int n=colors.size();
+        if(n<=2) 
+            return false;
+        int a=0,b=0;
+        for(int i=1;i<n-1;i++){
+            if(colors[i]=='A' && colors[i-1]=='A' && colors[i+1]=='A') 
+                a++;
+            if(colors[i]=='B' && colors[i-1]=='B' && colors[i+1]=='B') 
+                b++;
+        }
+        return a>b;
+    }
